e033c7356a065781a20cca7b278c8ec1bc71796c
ankitgithubcracker <112914308+ankitgithubcracker@users.noreply.github.com>
2022-10-17 00:58:46 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,21 @@
+let string=""
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }else  if(e.target.innerHTML == 'C' || e.target.innerHTML == 'AC'){
+            string = "";
+            document.querySelector('input').value = string;
+        }else  if(e.target.innerHTML == '%'){
+            string = eval(string/100);
+            document.querySelector('input').value = string;
+        }else{
+        console.log(e.target);
+        string += e.target.innerHTML;
+        document.querySelector('input').value = string;
+    }
+
+    })
+})
